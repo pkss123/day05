fe1764a4c79a6caf2f3945ef33f6d2c11c26e619
pkss123 <80875171+pkss123@users.noreply.github.com>
2021-05-17 21:53:24 +0900

    Add files via upload
    C언어 5일차

---
+++ day05.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+
+int main() {
+	//int sc1, sc2, sc3, sum;
+	//double avg;
+	//
+	//printf("세 과목 점수 입력 : ");
+	//scanf_s("%d %d %d", &sc1, &sc2, &sc3);
+
+	//sum = sc1 + sc2 + sc3;
+	//avg = (double)sum / 3;
+
+	//printf("세 과목 : %d 점 %d 점 %d 점\n총점 : %d 점 \n 평균 : %.2f\n", sc1, sc2, sc3, sum, avg);
+
+	/*
+		제어문
+		 코드의 흐름을 제어할 수 있는 함수
+
+		if문
+		 조건문 - 조건식에 따라 코드의 실행 순서가 결정되는 제어문
+
+		if ( 조건식 ) {
+			조건식이 참이면 실행될 코드
+		}
+
+		조건식이 참이면 if 내부의 코드를 실행한 후 다음 코드 진행
+		조건식이 거짓이면 if문 전체를 건너띄고 하위 코드를 진행
+
+			* 모든 제어문은 한 줄 까지는
+			2줄 이상의 코드는 중괄호를 이용하여 제어문의 범위를 지정
+	*/
+
+	//int n1, n2;
+	//printf("정수 두 개 입력 : ");
+	//scanf_s("%d %d", &n1, &n2);
+
+	//if (n1 > n2) {
+	//	printf("n1이 더 크다\n");
+	//}
+	//printf("다음 문장 진행\n");
+
+	//int su1, su2;
+	//printf("숫자 두 개 입력 : "); scanf_s("%d %d", &su1, &su2);
+
+	//if (su1 > su2)
+	//	printf("%d가 더 크다\n", su1);
+
+	//if (su1 < su2)
+	//	printf("%d가 더 크다\n", su2);
+
+	//int su;
+
+	//printf("숫자 입력 : "); scanf_s("%d", &su);
+
+	//if (su % 2 == 0)
+	//	printf("입력한 숫자 : %d -> 짝수이다\n", su);
+	//if (su % 2 != 0) 
+	//	printf("입력한 숫자 : %d -> 홀수이다\n", su);
+
+	/*
+	
+	*/
+
+	//int su;
+
+	//printf("숫자 입력 : "); scanf_s("%d", &su);
+
+	//if (su>0 && su % 2 == 0)
+	//	printf(" %d : 양수이면서 짝수이다\n", su);
+	//
+	//int su;
+
+	//printf("숫자 입력 : "); scanf_s("%d", &su);
+
+	//if (su % 5 == 0)
+	//	printf(" %d : 5의 배수이다\n", su);
+	//
+
+	int su1, su2, su3;
+
+	//printf("세 개의 숫자 입력 : "); scanf_s("%d %d %d", &su1, &su2, &su3);
+
+	//	if (su1 > 0 && su2 > 0 && su3 > 0 && su1 > su2 && su1 > su3){
+	//		printf("세 개의 숫자 중 가장 큰 수는 %d입니다\n", su1);
+	//	}
+	//	if (su1 > 0 && su2 > 0 && su3 > 0 && su2 > su1 && su2 > su3) {
+	//		printf("세 개의 숫자 중 가장 큰 수는 %d입니다\n", su2);
+	//	}
+	//	if (su1 > 0 && su2 > 0 && su3 > 0 && su3 > su1 && su3 > su2) {
+	//		printf("세 개의 숫자 중 가장 큰 수는 %d입니다\n", su3);
+	//	}
+
+	//int s1, s2, sum;
+
+	//printf("숫자 입력 : "); scanf_s("%d %d", &s1, &s2);
+
+	//sum = s1 + s2;
+
+	//printf("두 수의 합은 ");
+	//if (sum % 2 == 0 && sum % 3 == 0)
+	//	printf("%d 는 2의 배수이면서 3의 배수이다", sum);
+	//if (sum % 2 != 0 || sum % 3 != 0)
+	//	printf("%d 는 2의 배수가 아니거나 3의 배수가 아니다", sum);
+
+	//char gd;
+
+	//printf("성별 입력 : "); scanf_s("%c", &gd);
+
+	//if (gd == 'm')
+	//	printf("남성");
+	//if (gd == 'f')
+	//	printf("여성");
+
+}
